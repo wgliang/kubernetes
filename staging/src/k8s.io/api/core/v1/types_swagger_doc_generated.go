2db1837d732333f3744aa693ded36e90031cd6d7
Guoliang Wang <iamwgliang@gmail.com>
2019-05-05 12:32:03 +0800

    update unit test of Gt and Lt

--- staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
+++ staging/src/k8s.io/api/core/v1/types_swagger_doc_generated.go
@@ -1152,7 +1152,7 @@ func (NodeSystemInfo) SwaggerDoc() map[string]string {
 var map_NumericAwareSelectorRequirement = map[string]string{
 	"":         "A null or empty node selector term matches no objects. The requirements of them are ANDed. The TopologySelectorTerm type implements a subset of the NumericAwareSelectorRequirement.",
 	"key":      "The label key that the selector applies to.",
-	"operator": "Represents a key's relationship to a set of values. Valid operators are In, NotIn, Exists, DoesNotExist. Gt, and Lt.",
+	"operator": "Represents a key's relationship to a set of values. Valid operators are In, NotIn, Exists, DoesNotExist, Gt and Lt.",
 	"values":   "An array of string values. If the operator is In or NotIn, the values array must be non-empty. If the operator is Exists or DoesNotExist, the values array must be empty. If the operator is Gt or Lt, the values array must have a single element, which will be interpreted as an integer. This array is replaced during a strategic merge patch.",
 }
 
